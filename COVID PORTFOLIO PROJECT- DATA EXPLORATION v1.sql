2b5c2ec4bd21941be652b2e73c4addd5e22be316
Opeyemi Akingbohungbe <100061877+opeyemi-analytics@users.noreply.github.com>
2022-11-30 11:18:21 +0100

    Update and rename Covid Portfolio Project v1.sql to COVID PORTFOLIO PROJECT- DATA EXPLORATION v1.sql
     Added the title and  skills used

--- COVID PORTFOLIO PROJECT- DATA EXPLORATION v1.sql
+++ COVID PORTFOLIO PROJECT- DATA EXPLORATION v1.sql
@@ -1,3 +1,9 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 SELECT * 
 FROM portfolioproject..coviddeaths
 ORDER BY 3,4
